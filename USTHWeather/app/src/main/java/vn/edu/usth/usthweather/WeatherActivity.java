2202594c88410fa1428f67f9fd20a28ccd0e38bd
nmhieu132 <hieubui130220@gmail.com>
2021-10-12 23:11:53 +0700

    practical 7 submitted

--- USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class WeatherActivity extends AppCompatActivity {
-
+    private ViewPagerAdapter viewPagerAdapter;
+    private ViewPager2 viewPager2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        viewPagerAdapter= new ViewPagerAdapter(this);
+        viewPager2=findViewById(R.id.viewpager);
+        viewPager2.setAdapter(viewPagerAdapter);
 //        WeatherForecast firstFragment = new WeatherForecast();
 //        getSupportFragmentManager().beginTransaction().add(R.id.container,firstFragment).commit();
     }
